5f377c1831cff801612fe533a081d34454ecafb4
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-09 23:06:23 -0600

    chore: disable ssr and enable prerendering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
   import { i18n } from "$lib/i18n";
   import { ParaglideJS } from "@inlang/paraglide-sveltekit";
   let { children } = $props();
+
+  export const prerender = true;
+  export const ssr = false;
 </script>
 
 <ParaglideJS {i18n}>
