b33d442061fd3930784bf0863f07602825c23f78
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-09 23:13:36 -0600

    fix: move prerender and ssr configuration into a +layout.ts file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,6 @@
   import { i18n } from "$lib/i18n";
   import { ParaglideJS } from "@inlang/paraglide-sveltekit";
   let { children } = $props();
-
-  export const prerender = true;
-  export const ssr = false;
 </script>
 
 <ParaglideJS {i18n}>
